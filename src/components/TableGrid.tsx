348fcf0d4516afce6fc6ae84afec4322fb152625
Sokiraon <wuziye1234@gmail.com>
2021-04-10 21:10:16 +0800

    add add-row feature

--- src/components/TableGrid.tsx
+++ src/components/TableGrid.tsx
@@ -1,28 +1,50 @@
-import { Box, Button, createStyles, Icon, InputAdornment, makeStyles, Paper, TextField, Theme } from "@material-ui/core";
+import {
+  Box,
+  Button,
+  createStyles,
+  Dialog,
+  DialogTitle,
+  Icon,
+  InputAdornment,
+  makeStyles,
+  Paper,
+  TextField,
+  Theme,
+} from "@material-ui/core";
 import { ColDef, GridApi } from "ag-grid-community";
 import { AgGridReact } from "ag-grid-react";
 import React, { useState } from "react";
+import { TextField as FormTextField } from "formik-material-ui";
+import { Field, Form, Formik } from "formik";
+import { Alert } from "@material-ui/lab";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: { background: '#f7f7f7' },
+    root: { background: "#f7f7f7" },
     table: {
       margin: theme.spacing(2),
       marginTop: 0,
       marginBottom: theme.spacing(3),
-      width: 'auto',
-      height: '100%'
+      width: "auto",
+      height: "100%",
     },
     actions: {
       margin: theme.spacing(2),
       marginBottom: 0,
     },
     searchField: { height: 15 },
-  }),
+    textField: {
+      width: "260px",
+      margin: theme.spacing(1),
+      marginLeft: theme.spacing(3),
+    },
+    dialogActions: { margin: theme.spacing(3), float: "right" },
+  })
 );
 
 interface TableGridProps {
   data: object[];
+  setData: React.Dispatch<React.SetStateAction<object[]>>;
   colDefs: ColDef[];
 }
 
@@ -37,36 +59,41 @@ interface ActionButtonProps {
 const gridOptions = {
   animateRows: true,
   undoRedoCellEditing: true,
-  rowSelection: 'multiple',
+  rowSelection: "multiple",
   defaultColDef: {
     editable: true,
     sortable: true,
     filter: true,
     resizable: true,
-  }
+  },
 };
 
 const ActionButton = (props: ActionButtonProps) => {
   const { endIcon, color, onClick, disabled, text } = props;
   return (
-    <Button variant='contained' color={color ? color : 'primary'}
-      endIcon={endIcon ? <Icon>{endIcon}</Icon> : <Icon>{text}</Icon>} 
-      disableElevation onClick={onClick} disabled={disabled}
-      style={{ marginRight: 8, marginBottom: 8 }}>
-      { text }
+    <Button
+      variant="contained"
+      color={color ? color : "primary"}
+      endIcon={endIcon ? <Icon>{endIcon}</Icon> : <Icon>{text}</Icon>}
+      disableElevation
+      onClick={onClick}
+      disabled={disabled}
+      style={{ marginRight: 8, marginBottom: 8 }}
+    >
+      {text}
     </Button>
   );
 };
 
 export default function TableGrid(props: TableGridProps) {
   const classes = useStyles();
-  const { data, colDefs } = props;
+  const { data, setData, colDefs } = props;
   const [gridApi, setGridApi] = useState<GridApi>();
+  const [addOpen, setAddOpen] = useState(false);
   const [delDisabled, setDelDisabled] = useState(true);
 
   function updateDelete() {
-    if (gridApi?.getSelectedRows().length)
-      setDelDisabled(false);
+    if (gridApi?.getSelectedRows().length) setDelDisabled(false);
     else setDelDisabled(true);
   }
 
@@ -75,35 +102,104 @@ export default function TableGrid(props: TableGridProps) {
       for (let node of gridApi.getSelectedNodes())
         data.splice(node.childIndex, 1);
     gridApi?.applyTransaction({
-      remove: gridApi.getSelectedRows()
+      remove: gridApi.getSelectedRows(),
     });
   }
 
   return (
     <React.Fragment>
+      <Dialog open={addOpen} onClose={() => setAddOpen(false)}>
+        <DialogTitle>添加新行</DialogTitle>
+        <Alert severity="info" elevation={0}>
+          新数据将被添加到原有行的末尾。
+        </Alert>
+        <Formik
+          initialValues={Object.fromEntries(
+            colDefs.map((colDef) => [colDef.field, ""])
+          )}
+          onSubmit={(values) => {
+            gridApi?.applyTransaction({
+              add: [values],
+            });
+            setData([...data, values]);
+            setAddOpen(false);
+          }}
+        >
+          {({ submitForm }) => (
+            <Form>
+              {colDefs.map((colDef) => (
+                <Field
+                  component={FormTextField}
+                  name={colDef.field}
+                  label={colDef.field}
+                  variant="filled"
+                  className={classes.textField}
+                />
+              ))}
+              <div className={classes.dialogActions}>
+                <Button onClick={() => setAddOpen(false)} size="large">
+                  取消
+                </Button>
+                <Button size="large" color="primary" onClick={submitForm}>
+                  提交
+                </Button>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </Dialog>
       <Box className={classes.actions}>
-        <ActionButton endIcon='add' text='添加新行' />
-        <ActionButton color='secondary' text='撤销' endIcon='undo'
-          onClick={() => gridApi?.undoCellEditing()} />
-        <ActionButton text='重做' endIcon='redo' 
-          onClick={() => gridApi?.redoCellEditing()} />
-        <ActionButton color='secondary' text='删除行' endIcon='delete'
-          disabled={delDisabled} onClick={handleDelete} />
-        <ActionButton endIcon='launch' text='导出'
-          onClick={() => gridApi?.exportDataAsCsv()} />
-        <TextField variant='outlined' size='small'
+        <ActionButton
+          endIcon="add"
+          text="添加新行"
+          onClick={() => setAddOpen(true)}
+        />
+        <ActionButton
+          color="secondary"
+          text="撤销"
+          endIcon="undo"
+          onClick={() => gridApi?.undoCellEditing()}
+        />
+        <ActionButton
+          text="重做"
+          endIcon="redo"
+          onClick={() => gridApi?.redoCellEditing()}
+        />
+        <ActionButton
+          color="secondary"
+          text="删除行"
+          endIcon="delete"
+          disabled={delDisabled}
+          onClick={handleDelete}
+        />
+        <ActionButton
+          endIcon="launch"
+          text="导出"
+          onClick={() => gridApi?.exportDataAsCsv()}
+        />
+        <TextField
+          variant="outlined"
+          size="small"
+          placeholder="搜索任意关键字"
           InputProps={{
             classes: { input: classes.searchField },
             startAdornment: (
-              <InputAdornment position='start'>
+              <InputAdornment position="start">
                 <Icon>search</Icon>
               </InputAdornment>
             ),
-          }} onChange={e => gridApi?.setQuickFilter(e.target.value)} />
+          }}
+          onChange={(e) => gridApi?.setQuickFilter(e.target.value)}
+        />
       </Box>
       <Paper className={`ag-theme-material ${classes.table}`} elevation={2}>
-        <AgGridReact gridOptions={gridOptions} columnDefs={colDefs} rowData={data}
-          onGridReady={e => setGridApi(e.api)} onSelectionChanged={updateDelete} />
+        <AgGridReact
+          gridOptions={gridOptions}
+          columnDefs={colDefs}
+          rowData={data}
+          onGridReady={(e) => setGridApi(e.api)}
+          onSelectionChanged={updateDelete}
+        />
       </Paper>
     </React.Fragment>
   );
--- src/views/New.tsx
+++ src/views/New.tsx
@@ -1,11 +1,34 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router";
-import { Button, Grid, Header, Input, Message, Segment, Step } from "semantic-ui-react";
-import SwipeableViews from 'react-swipeable-views';
+import {
+  Button,
+  Grid,
+  Header,
+  Input,
+  Message,
+  Segment,
+  Step,
+} from "semantic-ui-react";
+import SwipeableViews from "react-swipeable-views";
 import Papa from "papaparse";
 import { FilePond } from "react-filepond";
 import { FilePondFile } from "filepond";
-import { MenuItem, TextField, Button as MTButton, Slide, Dialog, AppBar, Toolbar, IconButton, Icon, Typography, makeStyles, Theme, createStyles, Paper } from "@material-ui/core";
+import {
+  MenuItem,
+  TextField,
+  Button as MTButton,
+  Slide,
+  Dialog,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Icon,
+  Typography,
+  makeStyles,
+  Theme,
+  createStyles,
+  Paper,
+} from "@material-ui/core";
 import { TransitionProps } from "@material-ui/core/transitions/transition";
 import { useDispatch } from "react-redux";
 import { addTask } from "../store/taskSlice";
@@ -15,14 +38,14 @@ import TableGrid from "../components/TableGrid";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    appBar: { position: 'relative' },
+    appBar: { position: "relative" },
     title: { marginLeft: theme.spacing(2), flex: 1 },
-  }),
+  })
 );
 
 const Transition = React.forwardRef(function Transition(
   props: TransitionProps & { children?: React.ReactElement },
-  ref: React.Ref<unknown>,
+  ref: React.Ref<unknown>
 ) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
@@ -33,7 +56,7 @@ export default function New() {
   const dispatch = useDispatch();
 
   const [step, setStep] = useState(0);
-  const [name, setName] = useState('');
+  const [name, setName] = useState("");
   const [files, setFiles] = useState<File[]>([]);
   const [fileIndex, setFileIndex] = useState(0);
 
@@ -41,12 +64,11 @@ export default function New() {
   const [tableData, setTableData] = useState<object[]>([]);
   const [tableOpen, setTableOpen] = useState(false);
 
-  const steps = ['欢迎', '取个名字', '来点数据', '做些修改', '大功告成'];
+  const steps = ["欢迎", "取个名字", "来点数据", "做些修改", "大功告成"];
   const nextStep = () => setStep(step + 1);
   const prevStep = () => setStep(step - 1);
 
-  const checkAvailable = () => {
-  };
+  const checkAvailable = () => {};
 
   const iniTable = () => {
     if (files[fileIndex])
@@ -54,137 +76,208 @@ export default function New() {
         header: true,
         dynamicTyping: true,
         skipEmptyLines: true,
-        encoding: 'utf-8',
+        encoding: "utf-8",
         complete: (res) => {
           let data = res.data as object[];
           setTableData(data);
-          setColDefs(Object.keys(data[0]).map((value, index) => ({
-            field: value,
-            checkboxSelection: !index,
-            headerCheckboxSelection: !index,
-          })))
+          setColDefs(
+            Object.keys(data[0]).map((value, index) => ({
+              field: value,
+              checkboxSelection: !index,
+              headerCheckboxSelection: !index,
+            }))
+          );
           setTableOpen(true);
-        }
+        },
       });
   };
 
-  const updateFiles = (newFiles: FilePondFile[]) => setFiles(newFiles.map(fileItem => fileItem.file));
+  const updateFiles = (newFiles: FilePondFile[]) =>
+    setFiles(newFiles.map((fileItem) => fileItem.file));
 
   const handleSubmit = () => {
-    dispatch(addTask({
-      running: true,
-      name: name,
-      startTime: new Date().toLocaleString('zh-CN'),
-    }));
-    history.push('/dashboard');
+    dispatch(
+      addTask({
+        running: true,
+        name: name,
+        startTime: new Date().toLocaleString("zh-CN"),
+      })
+    );
+    history.push("/dashboard");
   };
 
   const setTableClose = () => setTableOpen(false);
   const saveTableData = () => {
-    let file = new File([new Blob(
-      [ Papa.unparse(tableData) ],
-      { type: 'text/csv' }
-    )], files[fileIndex].name);
+    let file = new File(
+      [new Blob([Papa.unparse(tableData)], { type: "text/csv" })],
+      files[fileIndex].name
+    );
     let newFiles = [...files];
     newFiles.splice(fileIndex, 1, file);
     setFiles(newFiles);
     setTableClose();
-  }
+  };
 
   return (
     <React.Fragment>
-      <Dialog fullScreen open={tableOpen} onClose={setTableClose} TransitionComponent={Transition}>
+      <Dialog
+        fullScreen
+        open={tableOpen}
+        onClose={setTableClose}
+        TransitionComponent={Transition}
+      >
         <Paper>
-          <AppBar className={classes.appBar} color='inherit' elevation={0}>
-            <Toolbar variant='dense'>
-              <IconButton edge='start' color='inherit' onClick={setTableClose}>
+          <AppBar className={classes.appBar} color="inherit" elevation={0}>
+            <Toolbar variant="dense">
+              <IconButton edge="start" color="inherit" onClick={setTableClose}>
                 <Icon>close</Icon>
               </IconButton>
-              <Typography variant='h6' className={classes.title}>查看及修改数据</Typography>
-              <MTButton color='inherit' onClick={saveTableData}>
-                <Typography variant='body1'>保存</Typography>
+              <Typography variant="h6" className={classes.title}>
+                查看及修改数据
+              </Typography>
+              <MTButton color="inherit" onClick={saveTableData}>
+                <Typography variant="body1">保存</Typography>
               </MTButton>
             </Toolbar>
           </AppBar>
         </Paper>
-        <TableGrid data={tableData} colDefs={colDefs} />
+        <TableGrid data={tableData} colDefs={colDefs} setData={setTableData} />
       </Dialog>
 
-      <Grid textAlign='center' style={{ height: '100vh'}} verticalAlign='middle'>
-        <Grid.Column width='9'>
-          <Step.Group ordered attached='top'>
-            { steps.map((value, index) => 
-            <Step key={value} title={value} completed={index < step}
-            active={index === step}  />
-            ) }
+      <Grid
+        textAlign="center"
+        style={{ height: "100vh" }}
+        verticalAlign="middle"
+      >
+        <Grid.Column width="9">
+          <Step.Group ordered attached="top">
+            {steps.map((value, index) => (
+              <Step
+                key={value}
+                title={value}
+                completed={index < step}
+                active={index === step}
+              />
+            ))}
           </Step.Group>
-          <Segment stacked textAlign='left' attached>
-            <SwipeableViews index={step} style={{ margin: '1em 0' }}>
+          <Segment stacked textAlign="left" attached>
+            <SwipeableViews index={step} style={{ margin: "1em 0" }}>
               <React.Fragment>
-                <Header as='h1' color='teal' content='欢迎！' />
-                <Typography variant='h6'>本向导将帮助你新建一个排产任务。</Typography>
-                <Typography variant='h6'>要继续，请单击“下一步”。</Typography>
+                <Header as="h1" color="teal" content="欢迎！" />
+                <Typography variant="h6">
+                  本向导将帮助你新建一个排产任务。
+                </Typography>
+                <Typography variant="h6">要继续，请单击“下一步”。</Typography>
               </React.Fragment>
 
               <React.Fragment>
-                <Header as='h3' content='首先，请确定一个任务名：' />
-                <Message info>
-                  <Message.Header>注意</Message.Header>
-                  <p>任务名不需要很复杂，具有足够辨识度即可。</p>
-                </Message>
-                <Input placeholder='输入任务名' action={
-                  <Button content='检查可用性' color='blue' onClick={checkAvailable}></Button>
-                } fluid onChange={e => setName(e.target.value)} />
+                <Header as="h3" content="首先，请确定一个任务名：" />
+                <Message
+                  info
+                  header="注意"
+                  content="任务名不需要很复杂，具有足够辨识度即可。"
+                />
+                <Input
+                  placeholder="输入任务名"
+                  action={
+                    <Button
+                      content="检查可用性"
+                      color="blue"
+                      onClick={checkAvailable}
+                    ></Button>
+                  }
+                  fluid
+                  onChange={(e) => setName(e.target.value)}
+                />
               </React.Fragment>
 
               <React.Fragment>
-                <Header as='h3' content='接下来，请上传数据文件：' />
-                <FilePond files={files} onupdatefiles={updateFiles} allowMultiple={true} maxFiles={5}
-                  labelIdle='拖放到此处 或 <span class="filepond--label-action">手动选择</span>' />
+                <Header as="h3" content="接下来，请上传数据文件：" />
+                <Message
+                  info
+                  header="提示"
+                  content="数据文件应当有5个，分别为订单信息、半成品信息、工序信息、资源信息及工序和资源的匹配关系。"
+                />
+                <FilePond
+                  files={files}
+                  onupdatefiles={updateFiles}
+                  allowMultiple={true}
+                  maxFiles={5}
+                  labelIdle='拖放到此处 或 <span class="filepond--label-action">手动选择</span>'
+                />
               </React.Fragment>
 
               <React.Fragment>
-                <Header as='h3' content='现在，你可以按你的意愿做些调整。' />
-                <Header as='h4' content='检查及修改数据' dividing />
+                <Header as="h3" content="现在，你可以按你的意愿做些调整。" />
+                <Header as="h4" content="检查及修改数据" dividing />
                 <Message warning>
                   <Message.Header>注意</Message.Header>
-                  <p>总体而言，我们建议你在专用的编辑器中处理数据，这通常会带来更好的效果。</p>
+                  <p>
+                    总体而言，我们建议你在专用的编辑器中处理数据，这通常会带来更好的效果。
+                  </p>
                 </Message>
-                <TextField select value={fileIndex} variant='outlined' size='small'
-                  onChange={(e) => setFileIndex(e.target.value as unknown as number)}
-                  style={{ width: '50%' }} label='选择文件'>
-                  {files.map((file, index) => 
-                  <MenuItem key={index} value={index}>{file.name}</MenuItem>
-                  )}
+                <TextField
+                  select
+                  value={fileIndex}
+                  variant="outlined"
+                  size="small"
+                  onChange={(e) =>
+                    setFileIndex((e.target.value as unknown) as number)
+                  }
+                  style={{ width: "50%" }}
+                  label="选择文件"
+                >
+                  {files.map((file, index) => (
+                    <MenuItem key={index} value={index}>
+                      {file.name}
+                    </MenuItem>
+                  ))}
                 </TextField>
-                <MTButton variant='contained' disableElevation 
-                onClick={() => iniTable()} size='large' 
-                style={{ marginLeft: '0.5em' }}>打开</MTButton>
-
-                <Header as='h4' content='自定义约束' dividing subheader='如果你对工序的前后置关系有自己的要求，请在此添加' />
+                <MTButton
+                  variant="contained"
+                  disableElevation
+                  onClick={() => iniTable()}
+                  size="large"
+                  style={{ marginLeft: "0.5em" }}
+                >
+                  打开
+                </MTButton>
               </React.Fragment>
-              
+
               <React.Fragment>
-                <Header as='h1' color='teal' content='完成！' />
-                <p>你已经完成了所有的准备工作，现在，你可以点击“提交”按钮，创建你的排产任务。</p>
-                <Header as='h2' content='之后会发生什么？' />
+                <Header as="h1" color="teal" content="完成！" />
+                <p>
+                  你已经完成了所有的准备工作，现在，你可以点击“提交”按钮，创建你的排产任务。
+                </p>
+                <Header as="h2" content="之后会发生什么？" />
                 <p>排产任务将被提交到服务器进行计算，这一过程可能较长。 </p>
-                <p>不过，不用担心，你随时可以在控制台查看现有的排产任务及其运行状态；
-                  如果你有了新的计划，也可以对现有任务追加订单或者创建新任务。</p>
+                <p>
+                  不过，不用担心，你随时可以在控制台查看现有的排产任务及其运行状态；
+                  如果你有了新的计划，也可以对现有任务追加订单或者创建新任务。
+                </p>
               </React.Fragment>
             </SwipeableViews>
-            <Button.Group widths='2' fluid>
-              <Button onClick={() => !step ? 
-                history.push('/dashboard') : prevStep()
-              }>{ !step ? '返回控制台' : '上一步'}</Button>
+            <Button.Group widths="2" fluid>
+              <Button
+                onClick={() =>
+                  !step ? history.push("/dashboard") : prevStep()
+                }
+              >
+                {!step ? "返回控制台" : "上一步"}
+              </Button>
               <Button.Or />
-              <Button positive onClick={() => step < steps.length-1 ?
-                nextStep() : handleSubmit()
-              }>{ step < steps.length-1 ? '下一步' : '提交'}</Button>
+              <Button
+                positive
+                onClick={() =>
+                  step < steps.length - 1 ? nextStep() : handleSubmit()
+                }
+              >
+                {step < steps.length - 1 ? "下一步" : "提交"}
+              </Button>
             </Button.Group>
           </Segment>
         </Grid.Column>
       </Grid>
     </React.Fragment>
-  )
+  );
 }
